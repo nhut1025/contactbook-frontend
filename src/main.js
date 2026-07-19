a2cebd3aaf7aa823f755a4b608d752fc2e542f50
DMNhut <ct7sxhctqk@privaterelay.appleid.com>
2026-07-19 22:21:29 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+createApp(App).use(router).mount('#app')
